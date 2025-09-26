66f9588dced86a0eac572712ccf5cd01d8c8016b
Abhiraj36 <emailabhiraj061@gmail.com>
2025-09-26 20:57:57 +0530

    Add subscription pre-save hook, error middleware, and schema fixes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // C:\Users\abhir_56d9u3c\Desktop\Subscription-Management-System\app.js
 
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from './config/env.js';
-import connectToDatabase from './database/mongodb.js';  // ✅ use default import
+import connectToDatabase from './database/mongodb.js';  
 
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/users.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import errorMiddleware from "./middlewares/error.middleware.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded ({ extended:false }));
+app.use(cookieParser());
 
 // Middleware
 app.use(express.json());
@@ -17,6 +23,8 @@ app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 // Root route
 app.get("/", (req, res) => {
